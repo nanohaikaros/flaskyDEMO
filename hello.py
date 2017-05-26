003e28a162f99b7d7231bb9ae17942549bdd8bd2
ikaros <1371689491@qq.com>
2017-05-26 13:47:18 +0800

    a3

--- hello.py
+++ hello.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from flask_script import Manager
 
 app = Flask(__name__)
+manager = Manager(app)
 
 @app.route('/')
 def index():
@@ -11,4 +13,4 @@ def user(name):
     return '<h1>Hello, %s!' % name
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    manager.run()
