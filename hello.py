7ef42c37f70ddbc6793815e5540731256e630ec2
ikaros <1371689491@qq.com>
2017-05-28 10:14:07 +0800

    flasky

--- hello.py
+++ hello.py
@@ -20,15 +20,14 @@ class NameForm(Form):
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    name = None
     form = NameForm()
     if form.validate_on_submit():
         old_name = session.get('name')
         if old_name is not None and old_name != form.name.data:
             flash('Looks like you have changed your name!')
-            return redirect(url_for('index'))
         session['name'] = form.name.data
-        return render_template('index.html', form=form, name=session.get('name'))
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'))
 
 
 @app.route('/user/<name>')
