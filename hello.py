d65f898cc1b9641dfae87829d2ff398d3fbad573
ikaros <1371689491@qq.com>
2017-05-28 10:07:16 +0800

    flasky

--- hello.py
+++ hello.py
@@ -26,9 +26,9 @@ def index():
         old_name = session.get('name')
         if old_name is not None and old_name != form.name.data:
             flash('Looks like you have changed your name!')
+            return redirect(url_for('index'))
         session['name'] = form.name.data
-        return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'))
+        return render_template('index.html', form=form, name=session.get('name'))
 
 
 @app.route('/user/<name>')
