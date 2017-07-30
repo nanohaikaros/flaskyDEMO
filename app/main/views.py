b6171c01b4bd3654b37c7cc2b72cffacdff44310
ikaros <1371689491@qq.com>
2017-07-30 09:26:09 +0800

    flask

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,10 @@
-from flask import render_template, abort
-from ..models import User
+from flask import render_template, abort, redirect, url_for, flash
+from flask_login import login_required, current_user
+from ..models import User, Role
 from . import main
+from .forms import EditProfileForm, EditProfileAdminForm
+from .. import db
+from ..decorators import admin_required
 
 
 @main.route('/')
@@ -13,3 +17,44 @@ def user(username):
     if user is None:
         abort(404)
     return render_template('user.html', user=user)
+
+@main.route('/edit-profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.name = form.name.data
+        current_user.location = form.location.data
+        current_user.about_me = form.about_me.data
+        flash('Your profile has been updated.')
+        return redirect(url_for('.user', username=current_user.username))
+    form.name.data = current_user.name
+    form.location.data = current_user.location
+    form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', form=form)
+
+@main.route('/edit-profile/<int:id>', methods=['GET', 'POST'])
+@login_required
+@admin_required
+def edit_profile_admin(id):
+    user = User.query.get_or_404(id)
+    form = EditProfileAdminForm(user=user)
+    if form.validate_on_submit():
+        user.email = form.email.data
+        user.username = form.username.data
+        user.confirmed = form.confirmed.data
+        user.role = form.query.get(form.role.data)
+        user.name = form.name.data
+        user.location = form.location.data
+        user.about_me = form.about_me.data
+        db.session.add(user)
+        flash('The profile has been updated.')
+        return redirect(url_for('.user', username=user.username))
+    user.email = user.email
+    user.username = user.username
+    user.confirmed = user.confirmed
+    user.role = user.role.id
+    user.name = user.name
+    user.location = user.location
+    user.about_me = user.about_me
+    return render_template('edit_profile.html', form=form, user=user)
