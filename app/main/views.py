8e1d68df1e06109ba89a10a06cf8ccad3ceb69d0
ikaros <1371689491@qq.com>
2017-07-31 21:19:02 +0800

    flask

--- app/main/views.py
+++ app/main/views.py
@@ -19,7 +19,7 @@ def index():
     page = request.args.get('page', 1, type=int)
     show_followed = False
     if current_user.is_authenticated:
-        show_followed = bool(request.cookoes.get('show_followed', ''))
+        show_followed = bool(request.cookies.get('show_followed', ''))
     if show_followed:
         query = current_user.followed_posts
     else:
@@ -35,11 +35,14 @@ def index():
 
 @main.route('/user/<username>')
 def user(username):
-    user = User.query.filter_by(username=username).first()
-    if user is None:
-        abort(404)
-        posts = user.posts.order_by(Post.timestamp.desc()).all()
-    return render_template('user.html', user=user)
+    user = User.query.filter_by(username=username).first_or_404()
+    page = request.args.get('page', 1, type=int)
+    pagination = user.posts.order_by(Post.timestamp.desc()).paginate(
+        page, per_page=current_app.config['FLASKY_POSTS_PER_PAGE'],
+        error_out=False)
+    posts = pagination.items
+    return render_template('user.html', user=user, posts=posts,
+                           pagination=pagination)
 
 @main.route('/edit-profile', methods=['GET', 'POST'])
 @login_required
@@ -116,6 +119,21 @@ def follow(username):
         return redirect(url_for('.user', username=username))
     current_user.follow(user)
     flash('You are now following %s' % username)
+    return redirect(url_for('.user', username=username))
+
+@main.route('/unfollow/<username>')
+@login_required
+@permission_required(Permission.FOLLOW)
+def unfollow(username):
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+        flash('Invalid user.')
+        return redirect(url_for('.index'))
+    if not current_user.is_following(user):
+        flash('You are not following this user.')
+        return redirect(url_for('.user', username=username))
+    current_user.unfollow(user)
+    flash('You are not following %s anymore.' % username)
     return redirect(url_for('.user', username=username))
 
 @main.route('/followers/<username>')
