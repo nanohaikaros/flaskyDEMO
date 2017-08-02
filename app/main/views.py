370239672ffe99f6ec4f767bff08cfd6f103265d
ikaros <1371689491@qq.com>
2017-08-02 20:34:27 +0800

    flask

--- app/main/views.py
+++ app/main/views.py
@@ -8,6 +8,17 @@ from .. import db
 from ..decorators import admin_required, permission_required
 
 
+@main.route('/shutdown')
+def server_shutdown():
+    if not current_app.testting:
+        abort(404)
+    shutdown = request.environ.get('werkzeug.server.shutdown')
+    if not shutdown:
+        abort(500)
+    shutdown()
+    return 'Shutting down...'
+
+
 @main.route('/', methods=['GET', 'POST'])
 def index():
     form = PostForm()
